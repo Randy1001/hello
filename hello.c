5d604dffbbe41023507ceb8adb0b021eceae12e4
Christopher Lariod <clarios2@huskers.unl.edu>
2022-08-25 23:24:59 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,12 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Christopher Larios\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
